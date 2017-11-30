2dbfa150f3ef83026ad87edbe253a5dc5a77cc1c
Csongor Szigeti <csongor.szigeti@gmail.com>
2017-11-30 13:46:35 +0100

    In line 14 merged the 'X' and 'x'. And cleared up the IF blocks with blank lines.

--- bowling.py
+++ bowling.py
@@ -7,22 +7,27 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
+
         if frame < 10  and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i].lower() == 'x':
                 result += get_value(game[i + 1])
+
                 if game[i + 2] == '/':
                     result += 10 - get_value(game[i + 1])
                 else:
                     result += get_value(game[i + 2])
+
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
+
         if in_first_half == True:
             in_first_half = False
         else:
             in_first_half = True
+            
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
