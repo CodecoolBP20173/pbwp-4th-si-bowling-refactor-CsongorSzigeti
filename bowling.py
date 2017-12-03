8c2fbec50bbdc9c3d1a78ed8cb2c45520d91b29e
Csongor Szigeti <csongor.szigeti@gmail.com>
2017-12-03 18:33:43 +0100

    'i' changed to 'rolling'

--- bowling.py
+++ bowling.py
@@ -14,32 +14,32 @@ def score(game):
     frame = 1
     in_first_half = True
 
-    for i in range(len(game)):
-        if game[i] == '/':
+    for rolling in range(len(game)):
+        if game[rolling] == '/':
             result += 10 - last
         else:
-            result += get_value(game[i])
+            result += get_value(game[rolling])
 
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i + 1])
-            elif game[i].lower() == 'x':
-                result += get_value(game[i + 1])
+        if frame < 10  and get_value(game[rolling]) == 10:
+            if game[rolling] == '/':
+                result += get_value(game[rolling + 1])
+            elif game[rolling].lower() == 'x':
+                result += get_value(game[rolling + 1])
 
-                if game[i + 2] == '/':
-                    result += 10 - get_value(game[i + 1])
+                if game[rolling + 2] == '/':
+                    result += 10 - get_value(game[rolling + 1])
                 else:
-                    result += get_value(game[i + 2])
+                    result += get_value(game[rolling + 2])
 
-        last = get_value(game[i])
+        last = get_value(game[rolling])
         
         if not in_first_half:
             frame += 1
 
         in_first_half = not in_first_half
 
-        if game[i].lower() == 'x':
+        if game[rolling].lower() == 'x':
             in_first_half = True
             frame += 1
-            
+
     return result
